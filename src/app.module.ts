d3ed22a49534fc2a68191fa406fa1f41d6a0eacb
Arash Rostami <arashrost4mi@gmail.com>
2024-01-13 13:34:09 +0330

    removing app controller and app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { SmsModule } from './sms/sms.module';
 import { EmailController } from './email/email.controller';
@@ -8,6 +7,6 @@ import { SmsController } from './sms/sms.controller';
 @Module({
   imports: [EmailModule, SmsModule],
   controllers: [EmailController, SmsController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
